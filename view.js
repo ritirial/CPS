b9f110bf0006874bee1d41e894cddd5b39654c5d
Ricardo T <ricardo@nucliux.mx>
2019-10-31 03:38:05 -0600

    photo copy rework

--- view.js
+++ view.js
@@ -2,13 +2,21 @@ let $ = require('jquery')
 let fs = require('fs')
 require('bootstrap')
 
-currPath = './reposit'
+var currPath = './reposit';
 
-importStringYear = ''
-importStringDay = ''
+if (fs.existsSync('R:/CPS/BUFFALO/IMAGES')) {
+  console.log('has requested path');
+  currPath = 'R:/CPS/BUFFALO/IMAGES';
+} else {
+  console.log('using local path');
+}
+
+var importStringYear = '';
+var importStringDay = '';
+
+var uploadFiles = [];
 
 function readPath() {
-  console.log(currPath)
   fs.readdir(currPath, (err, dir) => {
     $('#directory_items').html('')
     for (i = 0; i < dir.length; i++) {
@@ -29,137 +37,200 @@ function readPath() {
               '<p class="item__name">'+path+'</p>'+
             '</div>'
         }
-        $('#directory_items').append(itemString)
+        $('#directory_items').append(itemString);
       }
     }
-    setLinks()
+    setLinks();
   });
 }
 
 function goPathFromLink(newPath) {
-  currPath = newPath
-  readPath()
+  currPath = newPath;
+  readPath();
 }
 
 function goPath(newPath) {
-  currPath = currPath + '/' + newPath
-  readPath()
+  currPath = currPath + '/' + newPath;
+  readPath();
 }
 
+//set navbar links
 function setLinks() {
-  $('#addressLinks').html(
-    '<li class="breadcrumb-item">REPOSIT</li>'+
-    '<li class="breadcrumb-item">BUFFALO</li>')
-  folders = currPath.split('/')
-  i = 3
+  $('#addressLinks').html('<li class="breadcrumb-item">R:</li><li class="breadcrumb-item">CPS</li><li class="breadcrumb-item">BUFFALO</li>');
+  folders = currPath.split('/');
+  i = 3;
   if (i > folders.length) { //main directory is current address
-    $('#addressLinks').append('<li class="breadcrumb-item active">IMAGES</li>')
+    $('#addressLinks').append('<li class="breadcrumb-item active">IMAGES</li>');
   } else { //inside a sub-folder
-    $('#addressLinks').append('<li class="breadcrumb-item"><a href="#" onclick="goPathFromLink(\'./reposit\')">IMAGES</a></li>') //add main address
+    $('#addressLinks').append('<li class="breadcrumb-item"><a href="#" onclick="goPathFromLink(\'./reposit\')">IMAGES</a></li>'); //add main address
     for (i = 2; i < folders.length; i++) { //add sub-folders
-      crumb = ''
-      crumbPath = '.'
+      crumb = '';
+      crumbPath = '.';
       if (i == folders.length - 1) { //last item, no interactivity
-        crumb = '<li class="breadcrumb-item active">'+folders[i]+'</li>'
+        crumb = '<li class="breadcrumb-item active">'+folders[i]+'</li>';
       } else { //mid item, add interactivity
         for (j = 1; j <= i; j++) { //re-assemble path
-          crumbPath = crumbPath + '/' + folders[j]
+          crumbPath = crumbPath + '/' + folders[j];
         }
-        crumb = '<li class="breadcrumb-item"><a href="#" onclick="goPathFromLink(\''+crumbPath+'\')">'+folders[i]+'</a></li>'
+        crumb = '<li class="breadcrumb-item"><a href="#" onclick="goPathFromLink(\''+crumbPath+'\')">'+folders[i]+'</a></li>';
       }
-      $('#addressLinks').append(crumb)
+      $('#addressLinks').append(crumb);
     }
   }
 }
 
 function setUpload(place) {
-  importString = ''
+  var importString = '';
 
   //location
   if (place == 'Buffalo') {
-    importString = '51'
+    importString = '51';
   }
 
-  now = new Date()
-  start = new Date(now.getFullYear(), 0, 0)
+  now = new Date();
+  start = new Date(now.getFullYear(), 0, 0);
 
   //year
-  importString = importString + now.getFullYear().toString().substring(2)
-  importStringYear = importString
+  importString = importString + now.getFullYear().toString().substring(2);
+  importStringYear = importString;
 
-  diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000)
-  oneDay = 1000 * 60 * 60 * 24
-  day = Math.floor(diff / oneDay)
+  diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+  oneDay = 1000 * 60 * 60 * 24;
 
-  if (day < 10) {
-    day = '00' + day
-  } else if (day < 100) {
-    day = '0' + day
-  }
+  day = '00' + Math.floor(diff / oneDay);
+  day = day.substr(day.length-3);
 
   //day
-  importString = importString + day
-  importStringDay = importString
+  importString = importString + day;
+  importStringDay = importString;
 
-  $('#importDate').html(importString)
+  $('#importDate').html(importString);
+}
+
+function startUploadAction() {
+  
 }
 
 //submit file to new directories
-function startUpload() {
+function startUploadSubmit() {
   //get files
-  inputFiles = $('#filesInput')[0].files
+  var uploadFiles = $('#filesInput')[0].files;;
+
+  //clear submit list
+  var inputFiles = [];
+
+  var sampleIndex = $('input[name=sampleRadio]:checked').val();
+  for (i = 0; i < uploadFiles.length; i++) {
+    if (i == sampleIndex) {
+      inputFiles.unshift(uploadFiles[i]);
+    } else {
+      inputFiles.push(uploadFiles[i]);      
+    }
+  }
 
   //make year dir if not exists
   if (!fs.existsSync('./reposit/'+importStringYear)) {
-    fs.mkdir('./reposit/'+importStringYear)
+    fs.mkdir('./reposit/'+importStringYear, (err) => {
+      console.log('error',err);
+    })
   }
 
-  //make day dir if not exists
-  if (!fs.existsSync('./reposit/'+importStringYear+'/'+importStringDay)) {
-    fs.mkdir('./reposit/'+importStringYear+'/'+importStringDay)
+  var sampleNumber = "000" + $('#importSample').val();
+  var sampleNumber = sampleNumber.substr(sampleNumber.length-4);
+  var importStringSample = importStringDay + sampleNumber;
+
+  //upload pictures to buffalo-year folder
+  for (i = 0; i < inputFiles.length; i++) {
+    var sourceName = inputFiles[i].name;
+
+    var count = "_" + Number(i+1);
+    //count = count.substr(count.length-3);
+
+    var newName = 'img'+importStringSample+count+'.'+sourceName.split('.')[1];
+
+    copyFile(inputFiles[i].path, './reposit/'+importStringYear+'/'+newName);
   }
 
-  var importStringSample = importStringDay + $('#importSample').val()
+  var importStringBussiness = $('#importLine').val();
+
+  //make day dir in bussiness line if not exists
+  if (!fs.existsSync('./reposit/'+importStringBussiness+'/'+importStringDay)) {
+    fs.mkdir('./reposit/'+importStringBussiness+'/'+importStringDay, (err) => {
+      console.log('error',err);
+    })
+  }  
 
   //make sample dir if not exists
-  if (!fs.existsSync('./reposit/'+importStringYear+'/'+importStringDay+'/'+importStringSample)) {
-    fs.mkdir('./reposit/'+importStringYear+'/'+importStringDay+'/'+importStringSample)
+  if (!fs.existsSync('./reposit/'+importStringBussiness+'/'+importStringDay+'/'+importStringSample)) {
+    fs.mkdir('./reposit/'+importStringBussiness+'/'+importStringDay+'/'+importStringSample, (err) => {
+      console.log('error',err);
+    });
   }
 
+  //make photos dir if not exists
+  if (!fs.existsSync('./reposit/'+importStringBussiness+'/'+importStringDay+'/'+importStringSample+'/Sample_Photo')) {
+    fs.mkdir('./reposit/'+importStringBussiness+'/'+importStringDay+'/'+importStringSample+'/Sample_Photo', (err) => {
+      console.log('error',err);
+    });
+  }
+  if (!fs.existsSync('./reposit/'+importStringBussiness+'/'+importStringDay+'/'+importStringSample+'/Exhibit_Photo')) {
+    fs.mkdir('./reposit/'+importStringBussiness+'/'+importStringDay+'/'+importStringSample+'/Exhibit_Photo', (err) => {
+      console.log('error',err);
+    });
+  }
+
+  //upload pictures to bussiness line sub-folders
   for (i = 0; i < inputFiles.length; i++) {
-    sourceName = inputFiles[0].name
+    var count = "0" + Number(i+1);
+    count = count.substr(count.length-2);
 
-    var count = "00" + Number(i+1);
-    count = count.substr(count.length-3);
+    var sourceName = inputFiles[i].name;
 
-    newName = 'img'+importStringSample+count+'_Labeling.'+sourceName.split('.')[1]
-
-    copyFile(inputFiles[i].path, './reposit/'+importStringYear+'/'+importStringDay+'/'+importStringSample+'/'+newName)
+    if (i == 0) {
+      var newName = 'img'+importStringSample+count+'.'+sourceName.split('.')[1];
+      copyFile(inputFiles[i].path, './reposit/'+importStringBussiness+'/'+importStringDay+'/'+importStringSample+'/Sample_Photo/'+newName);
+    } else {
+      var newName = 'img'+importStringSample+count+'_Labeling.'+sourceName.split('.')[1];
+      copyFile(inputFiles[i].path, './reposit/'+importStringBussiness+'/'+importStringDay+'/'+importStringSample+'/Exhibit_Photo/'+newName);
+    }
   }
 
-  $('#exampleModal').modal('hide')
-  $('#filesInput').val('')
+  $('#exampleModal').modal('hide');
+}
+
+//open modal and clear inputs
+function clearInputs() {
+  $('#importSample').val('');
+  $('#importLine').val('EL');
+  $('#filesInput').val('');
+  $('#filesTable').html('');
 }
 
 //remote call the input
 function pressFiles() {
-  $('#filesInput').trigger('click')
+  $('#filesInput').trigger('click');
 }
 
-//show selected files in a list
-function clearInputs() {
-  $('#importSample').val('')
-  $('#checkEL').prop('checked', false).parent().removeClass('active');
-  $('#checkHBHF').prop('checked', false).parent().removeClass('active');
-  $('#checkHL').prop('checked', false).parent().removeClass('active');
-  $('#checkSTF').prop('checked', false).parent().removeClass('active');
-  $('#checkTY').prop('checked', false).parent().removeClass('active');
-}
-
+//has detected new files in the 
 $('#filesInput').change(function() {
+  var tableString = '';
 
+  var inputFiles = $('#filesInput')[0].files;
+
+  if (inputFiles.length > 0) {
+    tableString = '<thead><tr><td class="center">Selected file</td><td class="center">Sample</td></tr></thead><tbody>';
+  
+    for(i = 0; i < inputFiles.length; i++) {
+      tableString += '<tr><td>'+inputFiles[i].name+'</td><td class="center"><div class="form-check-inline"><label class="form-check-label"><input type="radio" class="form-check-input" name="sampleRadio" value="'+i+'"></label></div></td></tr>';
+    }
+
+    tableString += '</tbody>';
+  }
+    
+  $('#filesTable').html(tableString);
 });
 
+//copy data from origin path to target path
 function copyFile(source, target) {
   var rd = fs.createReadStream(source);
   var wr = fs.createWriteStream(target);
@@ -175,49 +246,8 @@ function copyFile(source, target) {
   });
 }
 
-readPath()
+//initial call to load the main view
+readPath();
 
-setUpload('Buffalo')
-
-/*
-$('#add-to-list').on('click', () => {
-   let name = $('#Name').val()
-   let email = $('#Email').val()
-
-   fs.appendFile('contacts', name + ',' + email + '\n')
-
-   addEntry(name, email)
-})
-
-function addEntry(name, email) {
-   if(name && email) {
-      sno++
-      let updateString = '<tr><td>'+ sno + '</td><td>'+ name +'</td><td>' 
-         + email +'</td></tr>'
-      $('#contact-table').append(updateString)
-   }
-}
-
-function loadAndDisplayContacts() {  
-   
-   //Check if file exists
-   if(fs.existsSync(filename)) {
-      let data = fs.readFileSync(filename, 'utf8').split('\n')
-      
-      data.forEach((contact, index) => {
-         let [ name, email ] = contact.split(',')
-         addEntry(name, email)
-      })
-   
-   } else {
-      console.log("File Doesn\'t Exist. Creating new file.")
-      fs.writeFile(filename, '', (err) => {
-         if(err)
-            console.log(err)
-      })
-   }
-
-   
-}
-
-loadAndDisplayContacts()*/
+//prepare upload form with buffalo data
+setUpload('Buffalo');
